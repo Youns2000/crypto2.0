3381166c8902d2e428416e1561bf2d4d4a0974df
Younes Benreguieg <younes@Younes-laptop.localdomain>
2021-12-20 23:35:35 +0100

    mercredi 29

--- main.py
+++ main.py
@@ -1,66 +1,146 @@
-from binance import AsyncClient, BinanceSocketManager
-import streamlit as st
-import asyncio
-import config
+import numpy as np
 import pandas as pd
+import config, account, currency, utils, usdt
+import json, asyncio, csv
+from binance import Client
+import streamlit as st
+import threading
+import traceback
+# import ptvsd
+# from thread_safe_st import ThreadSafeSt
 
-# async def kline_listener(client):
-#     bm = BinanceSocketManager(client)
-#     async with bm.kline_socket(symbol='BTCUSDT') as stream:
-#         while True:
-#             res = await stream.recv()
-#             # df = pd.DataFrame(res)
-#             print(res)
+# thread_safe_st = ThreadSafeSt()
 
-# async def kline_listener(client):
-#     bm = BinanceSocketManager(client)
-#     symbol = 'BNBBTC'
-#     res_count = 0
-#     async with bm.kline_socket(symbol=symbol, interval='1m') as stream:
-#         while True:
-#             res = await stream.recv()
-#             res_count += 1
-#             print(res)
-#             if res_count == 5:
-#                 res_count = 0
-#                 order_book = await client.get_order_book(symbol=symbol)
-#                 print(order_book)
+SLEEPING_TIME = 3
 
-async def order_book(client, symbol):
-    order_book = await client.get_order_book(symbol=symbol)
-    print(order_book)
+async def print_infos(total, acc, df, tab, logs):
+    while True:
+        t = await acc.total()
+        total.markdown("""
+        # TOTAL : %s EUR""" % t)
+        tab.table(df)
+        str = ""
+        with open('logs.txt', 'r') as f:
+            lines = f.readlines()
+            for l in lines:
+                str += l
+                str += '\n'
+            logs.markdown("""
+            # LOGS : \n %s """ % str)
+        a = await asyncio.sleep(SLEEPING_TIME)
 
+def between_update_files(acc):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-async def kline_listener(client):
-    bm = BinanceSocketManager(client)
-    symbol = 'BTCUSDT'
-#     res_count = 0
-#     async with bm.symbol_ticker_socket(symbol=symbol) as stream:
-#         # while True:
-#         res = await stream.recv()
-#         res_count += 1
-#         # df = pd.DataFrame(res)
-#         # print(df)
-#         print(res)
-#         # if res_count == 5:
-#         #     res_count = 0
-#         #     loop.call_soon(asyncio.create_task, order_book(client, symbol))
-    ts = bm.trade_socket(symbol=symbol)
-    # then start receiving messages
-    async with ts as tscm:
-        while True:
-            res = await tscm.recv()
-            print(res)
+    loop.run_until_complete(acc.update_all_files())
+    loop.close()
 
+async def set_acc():
+    # loading account data :
+    # - creating client
+    # - creating USDT_curr :
+    #           - reading the EURUSDT config file
+    # - update usdt data
+    # - add every curr that have a conf file
+    acc = await account.Account.create()
+    return acc
 
+async def close_acc(acc):
+    await acc.close()
 
-async def main():
-    client = await AsyncClient.create(config.API_KEY, config.API_SECRET)
-    await kline_listener(client)
-    await client.close_connection()
-
+async def add_curr(curr, acc):
+    if await acc.exist_curr(curr):
+        await acc.add_curr(curr)
+        st.write("Added", curr, "currency")
+    else:
+        st.error("Currency doesn't exist")
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
-    loop.run_until_complete(main())
+
+    ######### SETTING ACCOUNT ############
+
+    acc = loop.run_until_complete(set_acc())
+    print("Account Created !")
+
+    ######### SETTING STREAMLIT DISPLAY ZONES ############
+
+    total = st.empty()
+    tab = st.empty()
+    logs = st.empty()
+    print("Streamlit empty display zones total and tab created !")
+    
+    ######### ADD CURRENCY ############
+    with st.form("add"):
+        st.write("""
+        # Add Currency
+        """)
+        curr = st.text_input('Currency')
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            loop.run_until_complete(add_curr(curr, acc))
+
+    print("Add currency form created !")
+
+    ######### INIT CURRENCIES ############
+    dic = {'name': [], 'activated': [], 'money': []}
+    dic['name'].append('USDT')
+    dic['activated'].append(False)
+    dic['money'].append(acc.usdt_var.fake_money)
+    for c in acc.currencies:
+        dic['name'].append(utils.remove_suffix(c.name, 'USDT'))
+        dic['activated'].append(c.activated)
+        dic['money'].append(c.fake_money)
+    df = pd.DataFrame(dic)
+
+    st.success("Currencies initialized :")
+    print("Currencies initialized :")
+    print(df)
+
+    ######### STARTING THREADS ############
+    update_files_thread = threading.Thread(target=between_update_files, args=(acc,))
+    st.success("Update files thread created !")
+    print("Update files thread created !")
+    # print_info_thread = threading.Thread(target=between_print_infos, args=(total, acc, df, tab,))
+    # print("Print info thread created !")
+
+    for c in acc.currencies:
+        if c.activated:
+            c.thread.start()
+            st.success("%s thread started !" % c.name)
+            print("%s thread started !" % c.name)
+
+    try:
+        update_files_thread.start()
+    except Exception:
+        traceback.print_exc()
+    st.success("Update files thread started !")
+    print("Update files thread started !")
+
+    
+    # try:
+    #     print_info_thread.start()
+    # except Exception:
+    #     traceback.print_exc()
+    # print("Print info thread started !")
+
+    loop.run_until_complete(print_infos(total, acc, df, tab, logs))
+
+    ######### ENDING THREADS ############
+    for c in acc.currencies:
+        if c.activated:
+            c.thread.join()
+            st.success("%s thread killed !" % c.name)
+            print("%s thread killed !" % c.name)
+
+    update_files_thread.join()
+    st.success("Update files thread killed !" % c.name)
+    print("Update files thread killed !" % c.name)
+    # print_info_thread.join()
+    # print("Print info thread killed !" % c.name)
+
+    ######### CLOSE EVERYTHING #############
+
+    loop.run_until_complete(close_acc(acc))
     loop.close()
---
+++ sockets_template.py
@@ -0,0 +1,66 @@
+from binance import AsyncClient, BinanceSocketManager
+import streamlit as st
+import asyncio
+import config
+import pandas as pd
+
+# async def kline_listener(client):
+#     bm = BinanceSocketManager(client)
+#     async with bm.kline_socket(symbol='BTCUSDT') as stream:
+#         while True:
+#             res = await stream.recv()
+#             # df = pd.DataFrame(res)
+#             print(res)
+
+# async def kline_listener(client):
+#     bm = BinanceSocketManager(client)
+#     symbol = 'BNBBTC'
+#     res_count = 0
+#     async with bm.kline_socket(symbol=symbol, interval='1m') as stream:
+#         while True:
+#             res = await stream.recv()
+#             res_count += 1
+#             print(res)
+#             if res_count == 5:
+#                 res_count = 0
+#                 order_book = await client.get_order_book(symbol=symbol)
+#                 print(order_book)
+
+async def order_book(client, symbol):
+    order_book = await client.get_order_book(symbol=symbol)
+    print(order_book)
+
+
+async def kline_listener(client):
+    bm = BinanceSocketManager(client)
+    symbol = 'BTCUSDT'
+#     res_count = 0
+#     async with bm.symbol_ticker_socket(symbol=symbol) as stream:
+#         # while True:
+#         res = await stream.recv()
+#         res_count += 1
+#         # df = pd.DataFrame(res)
+#         # print(df)
+#         print(res)
+#         # if res_count == 5:
+#         #     res_count = 0
+#         #     loop.call_soon(asyncio.create_task, order_book(client, symbol))
+    ts = bm.trade_socket(symbol=symbol)
+    # then start receiving messages
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
+
+
+
+async def main():
+    client = await AsyncClient.create(config.API_KEY, config.API_SECRET)
+    await kline_listener(client)
+    await client.close_connection()
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
