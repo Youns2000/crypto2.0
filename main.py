bbbe0994b68aa5ebc0d2c4545ea2feece415b74f
Younes Benreguieg <younes.benreguieg@epita.fr>
2021-12-26 16:14:08 +0100

    normally understood everything

--- main.py
+++ main.py
@@ -4,36 +4,63 @@ import asyncio
 import config
 import pandas as pd
 
-# async def order_book(client, symbol):
-#     order_book = await client.get_order_book(symbol=symbol)
-#     print(order_book)
-
+# async def kline_listener(client):
+#     bm = BinanceSocketManager(client)
+#     async with bm.kline_socket(symbol='BTCUSDT') as stream:
+#         while True:
+#             res = await stream.recv()
+#             # df = pd.DataFrame(res)
+#             print(res)
 
 # async def kline_listener(client):
 #     bm = BinanceSocketManager(client)
 #     symbol = 'BNBBTC'
 #     res_count = 0
-#     async with bm.kline_socket(symbol=symbol) as stream:
+#     async with bm.kline_socket(symbol=symbol, interval='1m') as stream:
 #         while True:
 #             res = await stream.recv()
 #             res_count += 1
 #             print(res)
 #             if res_count == 5:
 #                 res_count = 0
-#                 loop.call_soon(asyncio.create_task, order_book(client, symbol))
+#                 order_book = await client.get_order_book(symbol=symbol)
+#                 print(order_book)
 
-async def funct():
+async def order_book(client, symbol):
+    order_book = await client.get_order_book(symbol=symbol)
+    print(order_book)
+
+
+async def kline_listener(client):
+    bm = BinanceSocketManager(client)
+    symbol = 'BTCUSDT'
+#     res_count = 0
+#     async with bm.symbol_ticker_socket(symbol=symbol) as stream:
+#         # while True:
+#         res = await stream.recv()
+#         res_count += 1
+#         # df = pd.DataFrame(res)
+#         # print(df)
+#         print(res)
+#         # if res_count == 5:
+#         #     res_count = 0
+#         #     loop.call_soon(asyncio.create_task, order_book(client, symbol))
+    ts = bm.trade_socket(symbol=symbol)
+    # then start receiving messages
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
+
+
+
+async def main():
     client = await AsyncClient.create(config.API_KEY, config.API_SECRET)
-    res = await asyncio.gather(
-        client.get_exchange_info(),
-        client.get_all_tickers()
-    )
-    # print(client)
-    print(res[0][0])
+    await kline_listener(client)
+    await client.close_connection()
 
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
-
-    loop.run_until_complete(funct())
+    loop.run_until_complete(main())
     loop.close()
